589cf5cea8617aa9bd467f94fed337eb58f724f0
ethan <e_posner@outlook.com>
2022-03-20 10:07:38 -0700

    Added user registration

--- src/users/urls.py
+++ src/users/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from users import views
 
 urlpatterns = [
-    # path("", views.login, name="login"),
     path("", include("django.contrib.auth.urls")),
     path("dashboard/", views.dashboard, name="dashboard"),
+    path("register/", views.register, name="register")
 ]
--- src/users/views.py
+++ src/users/views.py
@@ -1,11 +1,27 @@
 # users/views.py
 
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from users.forms import CustomUserCreationForm
 
-
-# Create your views here.
-def login(request):
-    return render(request, "registration/login.html")
 
 def dashboard(request):
-    return render(request, "users/dashboard.html")
+    if request.user.is_authenticated:
+        return render(request, "users/dashboard.html")
+    else:
+        return redirect(reverse("login"))
+
+
+def register(request):
+    if request.method == "GET":
+        return render(
+            request, "users/register.html",
+            {"form": CustomUserCreationForm}
+        )
+    elif request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(reverse("dashboard"))
