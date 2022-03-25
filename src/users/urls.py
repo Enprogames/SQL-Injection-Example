d4cce0f3b18d89e9b2482bd5db0b4d221b57dc7c
ethan <e_posner@outlook.com>
2022-03-25 09:34:55 -0700

    Created display for created documents

--- src/users/urls.py
+++ src/users/urls.py
@@ -6,5 +6,6 @@ from users import views
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
     path("dashboard/", views.dashboard, name="dashboard"),
-    path("register/", views.register, name="register")
+    path("register/", views.register, name="register"),
+    path('<int:document_id>/', views.detail, name='detail')
 ]
--- src/users/views.py
+++ src/users/views.py
@@ -4,11 +4,30 @@ from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from users.forms import CustomUserCreationForm
+from django.contrib.auth.models import User
+from users.models import Document
+
+from django.http import HttpResponse
+
+
+def detail(request, document_id):
+    if request.user.is_authenticated:
+        document = Document.objects.get(pk=document_id)
+        context = {
+            "title": document.title,
+            "content": document.content
+        }
+        return render(request, "users/detail.html", context)
+    else:
+        return redirect(reverse("login"))
 
 
 def dashboard(request):
     if request.user.is_authenticated:
-        return render(request, "users/dashboard.html")
+        context = {
+            "document_list": [{"id": document.id, "title": document.title} for document in request.user.document_set.all()]
+        }
+        return render(request, "users/dashboard.html", context)
     else:
         return redirect(reverse("login"))
 
